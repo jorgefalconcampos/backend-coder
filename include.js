6c9bff4e647212b28170b16a16b195163de0469c
Jorge Falcón <jorgef_campos@outlook.com>
2023-03-04 22:24:35 -0600

    repaso

---
+++ include.js
@@ -0,0 +1,5 @@
+const valoresBase = [1,2,3,4];
+
+const nuevosValores = valoresBase.map(x => x**2);
+
+console.log(nuevosValores);
---
+++ object.js
@@ -0,0 +1,14 @@
+const impuestos = {
+    impuesto1: 222,
+    impuesto: 341,
+    impuesto4: 2343,
+};
+
+let parClaveValor = Object.entries(impuestos); //devuelve un array de los
+let soloClave = Object.keys(impuestos);
+let soloValor = Object.values(impuestos);
+
+
+console.log(parClaveValor);
+console.log(soloClave);
+console.log(soloValor);
