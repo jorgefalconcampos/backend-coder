5c01ed0fe91212bdc794ecfe892e776f674ccd43
Jorge Falcón <jorgef_campos@outlook.com>
2023-03-12 19:16:18 -0600

    módulo fs

--- arch.js
+++ arch.js
@@ -2,48 +2,48 @@ const fs = require("fs");
 
 // síncrono
 
-// fs.writeFileSync("./data.txt", "THIS es el texto a guardar", "utf-8");
+fs.writeFileSync("./data.txt", "THIS es el texto a guardar", "utf-8");
 
-// fs.appendFileSync("./data.txt", "\nEsto es otro texto agregado con append", "utf8");
+fs.appendFileSync("./data.txt", "\nEsto es otro texto agregado con append", "utf8");
 
-// console.log(fs.existsSync("./data.txt"));
+console.log(fs.existsSync("./data.txt"));
 
-// const archivo = fs.readFileSync("./data.txt", "utf-8");
+const archivo = fs.readFileSync("./data.txt", "utf-8");
 
-// console.log(archivo);
+console.log(archivo);
 
-// fs.unlinkSync("./data.txt");
+fs.unlinkSync("./data.txt");
 
 
 
 
 // callbacks
 
-// fs.writeFileSync("./data.txt", "Esto es lo que agregamos al archivo", "utf-8", (err) => {
-//     if (err) {
-//         console.error("Hubo un error:" + err);
-//     }
-// });
+fs.writeFileSync("./data.txt", "Esto es lo que agregamos al archivo", "utf-8", (err) => {
+    if (err) {
+        console.error("Hubo un error:" + err);
+    }
+});
 
-// fs.readFile("./data.txt", "utf-8", (err, resultado) => {
-//     if (err) {
-//         console.log(err);
-//     }
+fs.readFile("./data.txt", "utf-8", (err, resultado) => {
+    if (err) {
+        console.log(err);
+    }
 
-//     console.log(resultado);
+    console.log(resultado);
 
-//     fs.appendFile("./data.txt", "\nAppend", "utf-8", (err) => {
+    fs.appendFile("./data.txt", "\nAppend", "utf-8", (err) => {
          
-//     });
-// });
+    });
+});
 
 
 
 // promesas
 
-// fs.promises.writeFile("./data.txt", "Creando y escribiendo el archivo", "utf-8")
-// .then(() => console.log("se escribió el archivo"))
-// .catch(err => console.log(err));
+fs.promises.writeFile("./data.txt", "Creando y escribiendo el archivo", "utf-8")
+.then(() => console.log("se escribió el archivo"))
+.catch(err => console.log(err));
 
 
 const manejoArchivo = async () => {
