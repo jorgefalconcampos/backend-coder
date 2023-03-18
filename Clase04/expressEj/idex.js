6c712bcf98c827c40d52e471603ae01f7c6f32b9
Jorge Falcón <jorgef_campos@outlook.com>
2023-03-17 23:27:14 -0600

    queryParams

--- Clase04/expressEj/idex.js
+++ Clase04/expressEj/idex.js
@@ -6,12 +6,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const users = [
-    { id: "1", nombre: "Nombre 1", apellido: "Appellido 1" },
-    { id: "2", nombre: "Nombre 2", apellido: "Appellido 2" },
-    { id: "3", nombre: "Nombre 3", apellido: "Appellido 3" },
-    { id: "4", nombre: "Nombre 4", apellido: "Appellido 4" },
-    { id: "5", nombre: "Nombre 5", apellido: "Appellido 5" },
-    { id: "6", nombre: "Nombre 6", apellido: "Appellido 6" },
+    { id: "1", nombre: "Nombre 1", apellido: "Appellido 1", genero: "M" },
+    { id: "2", nombre: "Nombre 2", apellido: "Appellido 2", genero: "M"  },
+    { id: "3", nombre: "Nombre 3", apellido: "Appellido 3", genero: "M" },
+    { id: "4", nombre: "Nombre 4", apellido: "Appellido 4", genero: "F" },
+    { id: "5", nombre: "Nombre 5", apellido: "Appellido 5", genero: "M" },
+    { id: "6", nombre: "Nombre 6", apellido: "Appellido 6", genero: "F" },
 ]
 
 app.get("/usuario/:nombre/:apellido", (req, res) => {
@@ -35,6 +35,23 @@ app.get("/usuario/:userId", (req, res) => {
 
 
 
+app.get("/api/query", (req, res) => {
+    console.log(req.query);
+    const { nombre, apellido } = req.query;
+    const { genero } = req.query;
+
+    let usuariosFiltrados = "";
+    if (!genero || (genero.toUpperCase() !== "F" && genero.toUpperCase() !== "M")) {
+        usuariosFiltrados = users;
+    }
+    else {   
+        usuariosFiltrados = users.filter((usuario) => {
+            // retorno explícito
+            return usuario.genero === genero;
+        });
+    }
+    res.send(usuariosFiltrados);   
+})
 
 
 
